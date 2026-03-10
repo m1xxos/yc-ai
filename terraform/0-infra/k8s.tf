1f2a8ef05e5dc7ef99ea219cae3ddd8669e6c0e9
m1xxos <66390094+m1xxos@users.noreply.github.com>
2026-03-10 23:01:22 +0300

    fix:bump size

--- terraform/0-infra/k8s.tf
+++ terraform/0-infra/k8s.tf
@@ -21,7 +21,7 @@ resource "selectel_mks_nodegroup_v1" "gpu_spot" {
   region                       = selectel_mks_cluster_v1.ai_cluster.region
   availability_zone            = "ru-9a"
   flavor_id                    = "3031" # GL3.4-32768-0-1GPU
-  volume_gb                    = 50
+  volume_gb                    = 100
   volume_type                  = "universal.ru-9a"
   nodes_count                  = 1
   install_nvidia_device_plugin = false
